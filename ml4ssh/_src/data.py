c096ee594da9702fa996e6c830178d79172ac854
Emmanuel <jemanjohnson34@gmail.com>
2022-05-31 11:39:39 +0200

    more efficient dl

--- ml4ssh/_src/data.py
+++ ml4ssh/_src/data.py
@@ -2,12 +2,19 @@ from typing import Optional, Iterator
 import tensorflow.data as tfd
 import jax.numpy as jnp
 import pandas as pd
+import tensorflow_datasets as tfds
+
+
+  # ds = ds.shuffle(buffer_size=10 * batch_size)
+  # ds = ds.batch(batch_size)
+  # ds = ds.prefetch(buffer_size=5)
+  # ds = ds.repeat()
 
 def make_mini_batcher(
     
     X, y,
     batch_size: Optional[int] = 32,
-    prefetch_buffer: Optional[int] = 1,
+    prefetch_buffer: Optional[int] = 5,
     shuffle: Optional[bool] = True
  ) -> Iterator:
 
@@ -17,19 +24,12 @@ def make_mini_batcher(
 
     # Make dataloader, set batch size and prefetch buffer:
     ds = tfd.Dataset.from_tensor_slices((X, y))
-    ds = ds.cache()
-    ds = ds.repeat()
     if shuffle:
         ds = ds.shuffle(buffer_size=batch_size)
     ds = ds.batch(batch_size)
     ds = ds.prefetch(prefetch_buffer)
+    ds = ds.repeat()
 
     # Make iterator:
-    train_iter = iter(ds)
-
-    # Batch loader:
-    def next_batch():
-        x_batch, y_batch = train_iter.next()
-        return jnp.asarray(x_batch.numpy()), jnp.asarray(y_batch.numpy())
-
-    return next_batch
+    ds = iter(tfds.as_numpy(ds))
+    return ds
