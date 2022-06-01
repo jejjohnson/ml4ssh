128dabec990befebc4705125a064b5e55e295340
jejjohnson <jemanjohnson34@gmail.com>
2022-06-01 22:53:40 +0200

    added more activations.

--- ml4ssh/_src/activations.py
+++ ml4ssh/_src/activations.py
@@ -8,12 +8,39 @@ class ReLU(eqx.Module):
     def __call__(self, x: Array) -> Array:
         return jax.nn.relu(x)
 
+
+class GeLU(eqx.Module):
+    """GELU activation (https://arxiv.org/abs/1606.08415) 
+    as used in Sparse Transformers (https://arxiv.org/abs/1904.10509)."""
+    def __call__(self, x: Array) -> Array:
+        return jax.nn.gelu(x)
+
+class ConcatelU(eqx.Module):
+    """Like concatenated ReLU (http://arxiv.org/abs/1603.05201), but with ELU instead."""
+    def __call__(self, x: Array) -> Array:
+        return jax.nn.relu(jnp.concatenate([x, -x], axis=0))
+
+
+class SoftPlus(eqx.Module):
+
+    def __call__(self, x: Array) -> Array:
+        return jax.nn.softplus(x)
+
 class Tanh(eqx.Module):
 
     def __call__(self, x: Array) -> Array:
         return jax.nn.tanh(x)
+
+class GatedTanh(eqx.Module):
+    dim: float = eqx.static_field()
+    def __init__(self, dim: int):
+        self.dim = dim
+    def __call__(self, x: Array) -> Array:
+        x_tanh, x_sigmoid = jnp.array_split(x, 2, axis=self.dim) 
+        return jax.nn.tanh(x_tanh) * jax.nn.sigmoid(x_sigmoid)
     
 class Swish(eqx.Module):
+    '''Swish activation (https://arxiv.org/abs/1710.05941).'''
     beta: float = eqx.static_field()
     
     def __init__(self, beta: float=1.0):
@@ -42,6 +69,8 @@ def get_activation(activation: str="relu", **kwargs):
         return ReLU()
     elif activation == "tanh":
         return Tanh()
+    elif activation == "softplus":
+        return SoftPlus()
     elif activation == "swish":
         return Swish(beta=kwargs.get("beta", 1.0))
     elif activation == "sine":
