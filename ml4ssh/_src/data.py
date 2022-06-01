bb8813bbd75b3a3bd6586a0e6647708e5dfec529
jejjohnson <jemanjohnson34@gmail.com>
2022-06-01 22:54:09 +0200

    working mini-batcher that's consistent.

--- ml4ssh/_src/data.py
+++ ml4ssh/_src/data.py
@@ -5,17 +5,13 @@ import pandas as pd
 import tensorflow_datasets as tfds
 
 
-  # ds = ds.shuffle(buffer_size=10 * batch_size)
-  # ds = ds.batch(batch_size)
-  # ds = ds.prefetch(buffer_size=5)
-  # ds = ds.repeat()
-
 def make_mini_batcher(
     
     X, y,
     batch_size: Optional[int] = 32,
     prefetch_buffer: Optional[int] = 5,
-    shuffle: Optional[bool] = True
+    shuffle: Optional[bool] = True,
+    buffer_size: Optional[int]=None
  ) -> Iterator:
 
     n = X.shape[0]
@@ -25,10 +21,22 @@ def make_mini_batcher(
     # Make dataloader, set batch size and prefetch buffer:
     ds = tfd.Dataset.from_tensor_slices((X, y))
     if shuffle:
-        ds = ds.shuffle(buffer_size=batch_size)
+        ds = ds.shuffle(buffer_size=10 * batch_size)
     ds = ds.batch(batch_size)
     ds = ds.prefetch(prefetch_buffer)
     ds = ds.repeat()
+
+    # # ALTERNATIVE (BUT SLOW :())
+    # # Make dataloader, set batch size and prefetch buffer:
+    # if buffer_size is None:
+    #     buffer_size = batch_size
+    # ds = tfd.Dataset.from_tensor_slices((X, y))
+    # ds = ds.cache()
+    # ds = ds.repeat()
+    # if shuffle:
+    #     ds = ds.shuffle(buffer_size=buffer_size)
+    # ds = ds.batch(batch_size)
+    # ds = ds.prefetch(prefetch_buffer)
 
     # Make iterator:
     ds = iter(tfds.as_numpy(ds))
