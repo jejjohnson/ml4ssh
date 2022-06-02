01f302990a0f366668540a270f5e45b39acd80bd
jejjohnson <jemanjohnson34@gmail.com>
2022-06-02 11:47:31 +0200

    updated scripts for GPU w/ less memory.

--- experiments/dc_2021b/demo_siren.py
+++ experiments/dc_2021b/demo_siren.py
@@ -13,15 +13,6 @@ sys.path.append(str(root))
 # current file
 filepath = os.path.dirname(__file__)
 
-#import tensorflow as tf
-# Ensure TF does not see GPU and grab all GPU memory.
-#tf.config.set_visible_devices([], device_type='GPU')
-# ENSURE JAX SEES GPU
-#os.environ["JAX_PLATFORM_NAME"] = "GPU"
-# ENSURE JAX DOESNT PREALLOCATE
-#os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = str(False)
-
-
 from pathlib import Path
 import argparse
 import wandb
--- experiments/dc_2021b/slurm_scripts/demo_siren_jz.sh
+++ experiments/dc_2021b/slurm_scripts/demo_siren_jz.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=dc21b                     # name of job
-#SBATCH --account=cli@v100                    # for statistics
-#SBATCH --nodes=1                           # we ALWAYS request one node
-#SBATCH --ntasks-per-node=1                 # number of tasks per node
+#SBATCH --account=cli@v100                   # for statistics
+#SBATCH --nodes=1                            # we ALWAYS request one node
+#SBATCH --ntasks-per-node=1                  # number of tasks per node
 #SBATCH --cpus-per-task=10                   # number of cpus per task
-#SBATCH -C v100-32g                 # V100 GPU + 32 GBs RAM
-#SBATCH --qos=qos_gpu-t3                   # GPU partition (max 100 hrs)
-#SBATCH --gres=gpu:1                 # number of GPUs (1/4 of GPUs)
+#SBATCH -C v100-16g                          # V100 GPU + 16 GBs RAM
+#SBATCH --qos=qos_gpu-t4                     # GPU partition (max 100 hrs)
+#SBATCH --gres=gpu:1                         # number of GPUs (1/4 of GPUs)
 #SBATCH --time=20:00:00                      # maximum execution time requested (HH:MM:SS)
 #SBATCH --output=/gpfswork/rech/cli/uvo53rl/logs/slurm/logs/ml4ssh_dc_2021b_%j.log      # name of output file
 #SBATCH --error=/gpfswork/rech/cli/uvo53rl/logs/slurm/errs/ml4ssh_dc_2021b_%j.err       # name of error file
@@ -39,8 +39,9 @@ export JAX_PLATFORM_NAME=GPU
 srun python experiments/dc_2021b/demo_siren.py \
     --wandb-mode offline \
     --log-dir /gpfswork/rech/cli/uvo53rl/logs \
-    --model mlp \
-    --n-epochs 800 \
+    --model siren \
+    --activation sine \
+    --n-epochs 1200 \
     --batch-size 4096 \
     --train-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/train \
     --ref-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/ref \
