77576a53722458badaf3a866ad7bb389d0070d30
Emmanuel <jemanjohnson34@gmail.com>
2022-06-02 14:29:20 +0200

    added longer script for cpu computation

--- experiments/dc_2021b/slurm_scripts/demo_siren_jz_cpu.sh
+++ experiments/dc_2021b/slurm_scripts/demo_siren_jz_cpu.sh
@@ -2,10 +2,11 @@
 
 #SBATCH --job-name=dc21b                     # name of job
 #SBATCH --account=cli@cpu                    # for statistics
+#SBATCH --qos=qos_cpu-t4                    # Maximum (100 hrs)
 #SBATCH --nodes=1                           # we ALWAYS request one node
 #SBATCH --ntasks-per-node=1                 # number of tasks per node
 #SBATCH --cpus-per-task=32                   # number of cpus per task
-#SBATCH --time=20:00:00                      # maximum execution time requested (HH:MM:SS)
+#SBATCH --time=48:00:00                      # maximum execution time requested (HH:MM:SS)
 #SBATCH --output=/gpfswork/rech/cli/uvo53rl/logs/slurm/logs/ml4ssh_dc_2021b_%j.log      # name of output file
 #SBATCH --error=/gpfswork/rech/cli/uvo53rl/logs/slurm/errs/ml4ssh_dc_2021b_%j.err       # name of error file
 #SBATCH --export=ALL
@@ -34,8 +35,11 @@ srun python experiments/dc_2021b/demo_siren.py \
     --wandb-mode offline \
     --log-dir /gpfswork/rech/cli/uvo53rl/logs \
     --model siren \
-    --n-epochs 500 \
-    --batch-size 2048 \
+    --n-epochs 2000 \
+    --activation sine \
+    --batch-size 4096 \
+    --learning-rate 1e-4 \
+    --julian-time False \
     --train-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/train \
     --ref-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/ref \
     --test-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/test
