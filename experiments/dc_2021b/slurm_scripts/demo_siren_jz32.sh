649f15ab1b58b533379b0aa2c3cc9087a6f01f64
jejjohnson <jemanjohnson34@gmail.com>
2022-06-03 16:04:46 +0200

    updated demo script.

--- experiments/dc_2021b/slurm_scripts/demo_siren_jz32.sh
+++ experiments/dc_2021b/slurm_scripts/demo_siren_jz32.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1                  # number of tasks per node
 #SBATCH --cpus-per-task=10                   # number of cpus per task
 #SBATCH -C v100-32g                          # V100 GPU + 16 GBs RAM
-#SBATCH --qos=qos_gpu-t3                     # GPU partition (max 20â hrs)
+#SBATCH --qos=qos_gpu-t3                     # GPU partition (max 20ï¿½ hrs)
 #SBATCH --gres=gpu:1                         # number of GPUs (1/4 of GPUs)
 #SBATCH --time=20:00:00                      # maximum execution time requested (HH:MM:SS)
 #SBATCH --output=/gpfsscratch/rech/cli/uvo53rl/logs/logs/ml4ssh_dc_2021b_%j.log      # name of output file
@@ -35,18 +35,33 @@ source activate jax_gpu_py39
 export XLA_PYTHON_CLIENT_PREALLOCATE=False
 export JAX_PLATFORM_NAME=GPU
 
+# # code execution
+# srun python experiments/dc_2021b/demo_siren.py \
+#     --wandb-mode offline \
+#     --log-dir /gpfsscratch/rech/cli/uvo53rl/logs \
+#     --model siren \
+#     --activation sine \
+#     --n-epochs 1200 \
+#     --batch-size 4096 \
+#     --hidden-dim 512 \
+#     --learning-rate 1e-4 \
+#     --train-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/train \
+#     --ref-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/ref \
+#     --test-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/test
+    
+
 # code execution
 srun python experiments/dc_2021b/demo_siren.py \
     --wandb-mode offline \
+    --smoke-test \
     --log-dir /gpfsscratch/rech/cli/uvo53rl/logs \
     --model siren \
     --activation sine \
     --n-epochs 1200 \
-    --batch-size 4096 \
+    --batch-size 300 \
     --hidden-dim 512 \
     --learning-rate 1e-4 \
     --train-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/train \
     --ref-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/ref \
     --test-data-dir /gpfsdswork/projects/rech/cli/uvo53rl/data/data_challenges/ssh_mapping_2021/test
     
-
