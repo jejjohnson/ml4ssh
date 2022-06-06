5ba6365ecbeb9e8c275207e8664da1df4c8eff4f
jejjohnson <jemanjohnson34@gmail.com>
2022-06-06 17:57:27 +0200

    added optimizer.

--- experiments/dc_2021b/demo_gp_torch.py
+++ experiments/dc_2021b/demo_gp_torch.py
@@ -48,6 +48,7 @@ from models.gp_torch import (
 from losses.gp_torch import add_loss_args, get_loss_fn
 from logger import add_logger_args
 from optimizers.gp_torch import add_optimizer_args
+from optimizers.LBFGS import FullBatchLBFGS
 from postprocess import add_postprocess_args, postprocess_data, generate_eval_data
 from evaluation import add_eval_args, get_rmse_metrics, get_psd_metrics
 
@@ -133,6 +134,71 @@ def main(args):
         n_devices = torch.cuda.device_count()
         output_device = torch.device("cuda:0")
         xtrain_tensor, ytrain_tensor = xtrain_tensor.cuda(output_device), ytrain_tensor.cuda(output_device)
+        
+        
+    class ExactGPModel(gpytorch.models.ExactGP):
+        def __init__(self, train_x, train_y, likelihood, n_devices):
+            super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+            self.mean_module = gpytorch.means.ConstantMean()
+            base_covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+            self.covar_module = gpytorch.kernels.MultiDeviceKernel(
+                base_covar_module, device_ids=range(n_devices),
+                output_device=output_device
+            )
+
+        def forward(self, x):
+            mean_x = self.mean_module(x)
+            covar_x = self.covar_module(x)
+            return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        
+        
+    def train(train_x,
+              train_y,
+              n_devices,
+              output_device,
+              checkpoint_size,
+              preconditioner_size,
+              n_training_iter,
+    ):
+        likelihood = gpytorch.likelihoods.GaussianLikelihood().to(output_device)
+        model = ExactGPModel(train_x, train_y, likelihood, n_devices).to(output_device)
+        model.train()
+        likelihood.train()
+
+        optimizer = FullBatchLBFGS(model.parameters(), lr=0.1)
+        # "Loss" for GPs - the marginal log likelihood
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+
+        with gpytorch.beta_features.checkpoint_kernel(checkpoint_size), \
+             gpytorch.settings.max_preconditioner_size(preconditioner_size):
+
+            def closure():
+                optimizer.zero_grad()
+                output = model(train_x)
+                loss = -mll(output, train_y)
+                return loss
+
+            loss = closure()
+            loss.backward()
+
+            for i in range(n_training_iter):
+                options = {'closure': closure, 'current_loss': loss, 'max_ls': 10}
+                loss, _, _, _, _, _, _, fail = optimizer.step(options)
+
+                print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+                    i + 1, n_training_iter, loss.item(),
+                    model.covar_module.module.base_kernel.lengthscale.item(),
+                    model.likelihood.noise.item()
+                ))
+
+                if fail:
+                    print('Convergence reached!')
+                    break
+
+        print(f"Finished training on {train_x.size(0)} data points using {n_devices} GPUs.")
+        return model, likelihood
         
     def find_best_gpu_setting(train_x,
                               train_y,
