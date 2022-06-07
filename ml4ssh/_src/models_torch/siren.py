7e099a51955ecc9c7795c5010c371f7f368018d6
jejjohnson <jemanjohnson34@gmail.com>
2022-06-07 14:48:10 +0200

    Added resnet skip connection to siren.

--- ml4ssh/_src/models_torch/siren.py
+++ ml4ssh/_src/models_torch/siren.py
@@ -41,6 +41,36 @@ class Siren(nn.Module):
         out = self.activation(out)
         return out
 
+class Siren(nn.Module):
+    def __init__(self, dim_in, dim_out, w0 = 1., c = 6., is_first = False, use_bias = True, activation = None, resnet = False):
+        super().__init__()
+        self.dim_in = dim_in
+        self.is_first = is_first
+
+        weight = torch.zeros(dim_out, dim_in)
+        bias = torch.zeros(dim_out) if use_bias else None
+        self.init_(weight, bias, c = c, w0 = w0)
+
+        self.weight = nn.Parameter(weight)
+        self.bias = nn.Parameter(bias) if use_bias else None
+        self.activation = Sine(w0) if activation is None else activation
+        self.resnet
+
+    def init_(self, weight, bias, c, w0):
+        dim = self.dim_in
+
+        w_std = (1 / dim) if self.is_first else (math.sqrt(c / dim) / w0)
+        weight.uniform_(-w_std, w_std)
+
+        if exists(bias):
+            bias.uniform_(-w_std, w_std)
+
+    def forward(self, x):
+        out =  F.linear(x, self.weight, self.bias)
+        out = self.activation(out)
+        if self.resnet:
+            out = x + out
+        return out
 
 class SirenNet(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 1., w0_initial = 30., use_bias = True, final_activation = None):
